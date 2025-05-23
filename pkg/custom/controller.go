2719361dc773dcef4974b7c039849179bef672b3
kallen <keleix@gmail.com>
2025-05-23 16:51:04 +0800

    feat: update custom collector

--- pkg/custom/controller.go
+++ pkg/custom/controller.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	apiruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	coreinformer "k8s.io/client-go/informers/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	corelister "k8s.io/client-go/listers/core/v1"
@@ -23,6 +24,8 @@ type LiteFinalizerController struct {
 	svcListerSynced cache.InformerSynced
 
 	queue workqueue.RateLimitingInterface
+
+	syncHandler func(ctx context.Context, key string) error
 }
 
 // NewLiteFinalizerController creates a new LiteFinalizerController instance
@@ -30,6 +33,20 @@ func NewLiteFinalizerController(
 	kubeClient clientset.Interface,
 	svcInformer coreinformer.ServiceInformer,
 ) *LiteFinalizerController {
+
+	// 增加 informer 监听事件
+	// hpaInformer.Informer().AddEventHandlerWithResyncPeriod(
+	// 	cache.ResourceEventHandlerFuncs{
+	// 		// 新增 HPA 回调方法
+	// 		AddFunc: hpaController.enqueueHPA,
+	// 		// 修改 HPA 回调方法
+	// 		UpdateFunc: hpaController.updateHPA,
+	// 		// 删除 HPA 回调方法
+	// 		DeleteFunc: hpaController.deleteHPA,
+	// 	},
+	// 	// 监听事件的定时周期
+	// 	resyncPeriod,
+	// )
 
 	svcInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj any) {
@@ -55,13 +72,31 @@ func NewLiteFinalizerController(
 // Run starts watching and syncing service finalizers
 func (lc *LiteFinalizerController) Run(ctx context.Context, workers int) {
 	defer apiruntime.HandleCrash()
+	defer lc.queue.ShutDown()
 
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-time.After(time.Second):
-
-		}
+	for range workers {
+		go wait.UntilWithContext(ctx, lc.worker, time.Second)
 	}
+	<-ctx.Done()
+}
+
+func (lc *LiteFinalizerController) worker(ctx context.Context) {
+	for lc.processNextWorkItem(ctx) {
+	}
+}
+
+func (lc *LiteFinalizerController) processNextWorkItem(ctx context.Context) bool {
+	key, quit := lc.queue.Get()
+	if quit {
+		return false
+	}
+	defer lc.queue.Done(key)
+
+	err := lc.syncHandler(ctx, key.(string))
+	lc.handleErr(ctx, err, key)
+	return true
+}
+
+func (lc *LiteFinalizerController) handleErr(ctx context.Context, err error, key any) {
+	// TODO
 }
